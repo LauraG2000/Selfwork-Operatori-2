4717ee363d98ef15e7be4fb0ace0e3f09e318853
LauraG2000 <lauragag19@gmail.com>
2024-12-13 15:34:23 +0100

    completato Selfwork Operatori 2

---
+++ OPERATORI2/Selwork-operatori2.js
@@ -0,0 +1,9 @@
+//anno di nascita
+let anno_di_nascita = 1960
+//anno corrente
+let anno_corrente = 2024
+// calcolo anni
+let età = anno_corrente - anno_di_nascita;
+let calcolo = 100 - età;
+
+console.log(`hai ${età} anni e te ne mancano ${calcolo} per compierne 100`);
